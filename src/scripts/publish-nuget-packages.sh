1b14d8dc13c15f15c1d4866c785c9c4ca2f1f215
Duncan McPherson <d.mcpherson.home@gmail.com>
2025-07-16 13:24:44 -0600

    fix: Linting and other miscommunication errors

--- src/scripts/publish-nuget-packages.sh
+++ src/scripts/publish-nuget-packages.sh
@@ -3,7 +3,7 @@
 $ARTIFACTS_DIR=$(circleci env subst "${ARTIFACTS_DIR}")
 $NUGET_API_KEY=$(circleci env subst "${NUGET_API_KEY}")
 
-dotnet nuget push $ARTIFACTS_DIR/*.nupkg \
+dotnet nuget push "$ARTIFACTS_DIR/*.nupkg" \
   --source "https://api.nuget.org/v3/index.json" \
-  --api-key $NUGET_API_KEY \
+  --api-key "$NUGET_API_KEY" \
   --skip-duplicate
