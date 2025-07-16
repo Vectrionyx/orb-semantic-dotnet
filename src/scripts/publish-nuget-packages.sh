d8985a5a7ec11fc1b92cf3d7acd8494eccfd71fc
Duncan McPherson <d.mcpherson.home@gmail.com>
2025-07-16 13:26:10 -0600

    fix: shell check errors

--- src/scripts/publish-nuget-packages.sh
+++ src/scripts/publish-nuget-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$ARTIFACTS_DIR=$(circleci env subst "${ARTIFACTS_DIR}")
-$NUGET_API_KEY=$(circleci env subst "${NUGET_API_KEY}")
+ARTIFACTS_DIR=$(circleci env subst "${ARTIFACTS_DIR}")
+NUGET_API_KEY=$(circleci env subst "${NUGET_API_KEY}")
 
 dotnet nuget push "$ARTIFACTS_DIR/*.nupkg" \
   --source "https://api.nuget.org/v3/index.json" \
